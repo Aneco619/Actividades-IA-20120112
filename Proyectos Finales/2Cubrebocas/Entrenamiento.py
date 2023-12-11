582b926feefcd584b7f2c6d5f338b0ea0d766245
Andrés G GS <aneco@192.168.1.12>
2023-12-10 22:56:59 -0600

    Actualizacion de nombre a carpeta para correrlo en el Cascade-Trainer-GUI

---
+++ Proyectos Finales/2Cubrebocas/Entrenamiento.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+import numpy as np
+
+direccion = '/Users/aneco/Documents/Instituto Tecnologico De Morelia/8vo Semestre/Inteligencia Artificial/Git Proyects/Proyectos Finales/2.Cubrebocas/Fotos'
+lista = os.listdir(direccion)
+
+etiquetas = []
+rostros = []
+con = 0
+
+for nameDir in lista:
+    nombre = direccion + '/' + nameDir
+
+    for fileName in os.listdir(nombre):
+        etiquetas.append(con)
+        rostros.append(cv2.imread(nombre + '/' + fileName,0))
+
+    con = con + 1
+
+reconocimiento = cv2.face.LBPHFaceRecognizer_create()
+
+reconocimiento.train(rostros, np.array(etiquetas))
+
+reconocimiento.write('ModeloLBP.xml')
+print("Modelo creado")
